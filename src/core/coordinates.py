c552b6c41dbfa41c2ce32fe51e0b068d645856b9
andrerag <andrerag@gmail.com>
2017-11-11 14:15:35 -0300

    Fixed variable names in coordinates class

--- src/core/coordinates.py
+++ src/core/coordinates.py
@@ -19,10 +19,10 @@ class Coordinates:
 		delta_phi = math.radians(coordinates.lat - self.lat)
 		delta_lambda = math.radians(coordinates.lng - self.lng)
 		
-		a = math.sin(delta_phi/2) * math.sin(delta_phi/2) +\
-			math.cos(phi1) * math.cos(phi2) *\
-			math.sin(delta_lambda/2) * math.sin(delta_lambda/2)
+		halfchord_square = math.sin(delta_phi/2) * math.sin(delta_phi/2) +\
+						   math.cos(phi1) * math.cos(phi2) *\
+						   math.sin(delta_lambda/2) * math.sin(delta_lambda/2)
 			
-		c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+		angular_distance = 2 * math.atan2(math.sqrt(halfchord_square), math.sqrt(1 - halfchord_square))
 		
-		return (earth_radius * c)
+		return (earth_radius * angular_distance)
