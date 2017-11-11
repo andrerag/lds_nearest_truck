2380465384e7710ed51e1358243d8575e5e5987c
andrerag <andrerag@gmail.com>
2017-11-11 20:07:24 -0300

    Changed cargo_truck_map variable name to truck_cargo_map for better code understading

--- src/core/cargo_truck_mapper.py
+++ src/core/cargo_truck_mapper.py
@@ -10,35 +10,35 @@ def map_cargos_to_trucks(cargo_list, trucks_bystate):
 	trucks_bystate_cpy = deepcopy(trucks_bystate)
 	cargo_list_cpy = deepcopy(cargo_list)
 
-	cargo_truck_map = defaultdict(list)
+	truck_cargo_map = defaultdict(list)
 
 	while not unique_cargo_to_trucks:
 		unique_cargo_to_trucks = True
 	
 		for curr_cargo in cargo_list_cpy:
 			truck, distance = truck_locator.find_nearest_truck(curr_cargo, trucks_bystate_cpy)
-			cargo_truck_map[truck].append((curr_cargo, distance))
+			truck_cargo_map[truck].append((curr_cargo, distance))
 
-		for curr_truck in cargo_truck_map:
-			if len(cargo_truck_map[curr_truck]) > 1:
-				remove_duplicates(curr_truck, cargo_truck_map, trucks_bystate_cpy)
+		for curr_truck in truck_cargo_map:
+			if len(truck_cargo_map[curr_truck]) > 1:
+				remove_duplicates(curr_truck, truck_cargo_map, trucks_bystate_cpy)
 				
 				trucks_bystate_cpy[curr_truck.state].remove(curr_truck)
-				cargo_list_cpy.remove(cargo_truck_map[curr_truck][0][0])
+				cargo_list_cpy.remove(truck_cargo_map[curr_truck][0][0])
 
 				unique_cargo_to_trucks = False
 
-	return	cargo_truck_map, get_total_distance(cargo_truck_map)
+	return	truck_cargo_map, get_total_distance(truck_cargo_map)
 
-def remove_duplicates(truck, cargo_truck_map, trucks_bystate):
-	(furthest_cargo, furthest_distance) = cargo_truck_map[truck][0]
+def remove_duplicates(truck, truck_cargo_map, trucks_bystate):
+	(furthest_cargo, furthest_distance) = truck_cargo_map[truck][0]
 	
-	for (curr_cargo, curr_distance) in cargo_truck_map[truck]:
+	for (curr_cargo, curr_distance) in truck_cargo_map[truck]:
 		if curr_distance > furthest_distance:
-			cargo_truck_map[truck].remove((furthest_cargo, furthest_distance))
+			truck_cargo_map[truck].remove((furthest_cargo, furthest_distance))
 			(furthest_cargo, furthest_distance) = (curr_cargo, curr_distance)
 		else:
-			cargo_truck_map[truck].remove((curr_cargo, curr_distance))
+			truck_cargo_map[truck].remove((curr_cargo, curr_distance))
 
-def get_total_distance(cargo_truck_map):
+def get_total_distance(truck_cargo_map):
 	return 0;
