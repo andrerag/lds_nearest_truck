03c38a392c993f1f766d675cd30a1af1a14777db
andrerag <andrerag@gmail.com>
2017-11-12 10:53:19 -0300

    Added comments to the coordinates.py file

--- src/core/coordinates.py
+++ src/core/coordinates.py
@@ -1,19 +1,41 @@
 import math
 
-class DistanceMeasurement:
-	EUCLIDIAN = 0
-	HAVERSINE = 1
-
 class Coordinates:
+	"""Contains lattitude and longitude information
+	
+	This class is not just a C-like struct object. The Coordinates class is also able to
+	calculate the distance from it's lat, lng information to another Coordinates object.
+
+	Attributes:
+		lat: Object's lattitude
+		lng: Object's longitude
+
+	"""
 	def __init__(self, lat, lng):
+		"""Constructor for the Coordinates class"""
 		self.lat = float(lat)
 		self.lng = float(lng)
 	
-	def distance_to(self, coordinates, type=DistanceMeasurement.HAVERSINE):
-		if type == DistanceMeasurement.HAVERSINE:
-			return self.__haversine_distance(coordinates)
+	def distance_to(self, coordinates):
+		"""Calculates the distance to another Coordinates object
+
+		Args:
+			coordinates: Coordinates object to measure distance to.
+		"""
+		return self.__haversine_distance(coordinates)
 
 	def __haversine_distance(self, coordinates):
+		"""Calculates distance using the haversine formula
+
+			This function calculates distance using latitude and longitude information using the
+			haversine formula, which taks into account the earth's curvature.
+
+			Args:
+				coordinates: Coordinates object to measure distance to.
+
+			Returns:
+				Distance in Km to given Coordinates object
+		"""
 		earth_radius = float(6371e3)
 		phi1 = math.radians(coordinates.lat)
 		phi2 = math.radians(self.lat)		
