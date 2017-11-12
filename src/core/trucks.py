db189a3b0998d4f714e630c38722f11374991093
andrerag <andrerag@gmail.com>
2017-11-12 11:43:02 -0300

    Added comments for trucks.py class

--- src/core/trucks.py
+++ src/core/trucks.py
@@ -1,13 +1,23 @@
 from core.coordinates import Coordinates
 
 class Truck:
+    """ This class represents a single Truck. It contains truck description and it's location
+    
+        Attributes:
+            truck: Truck description
+            city: City where the truck is located
+            state: State where the truck is located
+            location: Coordinates object containing latitude and longitude
+    """
 	def __init__(self, truck, city, state, lat, lng):
+		"""Constructor for Truck class"""
 		self.truck = truck
 		self.city  = city
 		self.state = state
 		self.location = Coordinates(lat, lng)
 
 	def __hash__(self):
+		"""Hash function so the Truck class can be used as a dictionary key"""
 		return hash(self.truck)
 
 	def __eq__(self, other):
