e4ee6f853c1875428287d6160256f455ca1c17df
andrerag <andrerag@gmail.com>
2017-11-12 16:29:30 -0300

    Added property checks

--- src/core/trucks.py
+++ src/core/trucks.py
@@ -1,6 +1,8 @@
 from core.coordinates import Coordinates
+from utils.neighbour_states import NEIGHBOURS
+from utils.neighbour_states import InvalidUSState
 
-class Truck:
+class Truck(object):
 	""" This class represents a single Truck. It contains truck description and it's location
 
 	    Attributes:
@@ -13,7 +15,7 @@ class Truck:
 		"""Constructor for the Truck class"""
 		self.truck = truck
 		self.city  = city
-		self.state = state
+		self._state = state
 		self.location = Coordinates(lat, lng)
 
 	def __hash__(self):
@@ -22,3 +24,21 @@ class Truck:
 
 	def __eq__(self, other):
 		return self.truck == other.truck
+
+	@property
+	def state(self):
+		return self._state
+
+	@state.setter
+	def state(self, value):
+		"""Setter method for the US state info
+
+		Args:
+			value: Valid US State
+
+		Raises:
+			InvalidUSState: The state given does not belog to the US
+		"""
+		if value not in NEIGHBOURS:
+			raise InvalidUSState("Given state does not belong to the United States")
+		self._state = value
