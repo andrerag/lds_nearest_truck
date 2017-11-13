cd797398660ce7ff7db9dce93c08579593079259
André Guimarães <andrerag@gmail.com>
2017-11-13 14:55:10 -0300

    Update and rename coordinates_test.py to test_coordinates.py
    Renamed test file so it can actually be executed using python's unittest framework

---
+++ src/core/test/test_coordinates.py
@@ -0,0 +1,25 @@
+import unittest
+from core.coordinates import Coordinates
+from core.coordinates import InvalidCoordinate
+
+class CoordinatesTest(unittest.TestCase):
+		def test_create_coordinate(self):
+			coord_str  = Coordinates('34.79981', '-87.677251')
+			coord_numb = Coordinates(34.79981, -87.677251)
+			
+			self.assertEqual(coord_str.lat, 34.79981)
+			self.assertEqual(coord_str.lng, -87.677251)
+			self.assertEqual(coord_numb.lat, 34.79981)
+			self.assertEqual(coord_numb.lng, -87.677251)
+			
+		def test_invalid_lattitude(self):
+			with self.assertRaises(InvalidCoordinate):
+				coord = Coordinates(-91.0, -30.0)
+			with self.assertRaises(InvalidCoordinate):
+				coord = Coordinates(91.0, -30.0)
+				
+		def test_invalid_longitude(self):
+			with self.assertRaises(InvalidCoordinate):
+				coord = Coordinates(-34.0, -181.0)
+			with self.assertRaises(InvalidCoordinate):
+				coord = Coordinates(-34.0, 181.0)
