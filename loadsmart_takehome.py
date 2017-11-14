004281dc7524d0d3304b3230497bba804df4edfa
André Guimarães <andrerag@gmail.com>
2017-11-14 11:26:20 -0300

    Update loadsmart_takehome.py

--- loadsmart_takehome.py
+++ loadsmart_takehome.py
@@ -3,15 +3,16 @@ from optparse import OptionParser
 
 from core.cargo_truck_mapper import CargoTruckMapper
 from utils import csv_parser
-from plotter import map_plotter
 
 if __name__ == "__main__":
-    usage = "usage: %prog --trucks=<CSVFILE> --cargo=<CSVFILE>"
+    usage = "usage: %prog --trucks=<CSVFILE> --cargo=<CSVFILE> --output=csv|html"
     cmd_args_parser = OptionParser(usage=usage)
     cmd_args_parser.add_option("--trucks", dest="trucks_csvfile", 
                                help=".csv File containing trucks locations")
     cmd_args_parser.add_option("--cargos", dest="cargos_csvfile", 
                                help=".csv File containing cargo locations and destination")
+    cmd_args_parser.add_option("--output", dest="output_type", 
+                               help="Choose 'csv' or 'html' to have a visual result using Google Maps")
     
     (options, args) = cmd_args_parser.parse_args()
     if not(options.trucks_csvfile) or not(options.cargos_csvfile):
@@ -23,10 +24,17 @@ if __name__ == "__main__":
     truck_list = csv_parser.load_trucks_bystate(options.trucks_csvfile)
     cargo_list = csv_parser.load_cargo_list(options.cargos_csvfile)
     
-    mapper = CargoTruckMapper(cargo_list, truck_list)
-
     logging.info("Mapping cargo to trucks")
+    mapper = CargoTruckMapper(cargo_list, truck_list)
     cargo_truck_mapping = mapper.map_cargos_to_trucks()
 
-    logging.info("Plotting results to results.html")
-    map_plotter.plot_results(options.cargos_csvfile, options.trucks_csvfile, cargo_truck_mapping)
+    if (not (options.output_type)) or (options.output_type == 'csv'):
+        logging.info("Writing results to results.csv")
+        csv_parser.generate_csv_results('results.csv', cargo_truck_mapping)
+    elif options.output_type == 'html':
+        from plotter import map_plotter
+        logging.info("Plotting results to results.html")
+        map_plotter.plot_results(options.cargos_csvfile, options.trucks_csvfile, cargo_truck_mapping)
+    else:
+        logging.info("Writing results to results.csv")
+        csv_parser.generate_csv_results('results.csv', cargo_truck_mapping)    
