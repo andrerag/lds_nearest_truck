8fff0dbe99f7e60c5ecc141cfe33d068349c3e54
andrerag <andrerag@gmail.com>
2017-11-14 01:25:46 -0300

    Added unit tests for truck locator. Caught a bug while doing that

--- core/test/test_truck_locator.py
+++ core/test/test_truck_locator.py
@@ -1,1 +1,94 @@
+import unittest
+from collections import defaultdict
+
+from core.truck import Truck
+from core.truck_locator import TruckLocator
+from core.cargo import Cargo
+
+class TruckLocatorTest(unittest.TestCase):
+	def setUp(self):
+		self.cargo = Cargo('Pandeiro', 'Fremont', 'WY', 42.870041, -107.744649,
+					  'Boston', 'MA', 42.365167, -71.084856)
+
+	def test_trucks_in_state(self):
+		truck1 = Truck('RecifeTransport', 'County1', 'WY', 44.650855, -104.687268) #Farthest
+		truck2 = Truck('RecifeTransport', 'County2', 'WY', 44.022229, -105.478284) #Near
+		truck3 = Truck('RecifeTransport', 'County3', 'WY', 43.562262, -106.071546) #Close
+		truck4 = Truck('RecifeTransport', 'County4', 'WY', 42.825402, -107.631604) #Really close
+		
+		trucks_bystate = {'WY': [truck1]}
+
+		locator = TruckLocator(trucks_bystate)
+
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck1)
+
+		trucks_bystate['WY'].append(truck2)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck2)
+		trucks_bystate['WY'].append(truck3)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck3)
+		trucks_bystate['WY'].append(truck4)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck4)
+
+	def test_trucks_in_state_and_neighbours(self):
+		truck1 = Truck('RecifeTransport', 'County1', 'WY', 44.650855, -104.687268) #Farthest
+		truck2 = Truck('RecifeTransport', 'County2', 'WY', 44.022229, -105.478284) #Near
+		truck3 = Truck('RecifeTransport', 'County3', 'WY', 43.562262, -106.071546) #Close
+		truck4 = Truck('RecifeTransport', 'County4', 'WY', 42.825402, -107.631604) #Really close
+
+		truck5 = Truck('RecifeTransport', 'County5', 'MT', 47.766819, -107.983167) #Other State
+		truck6 = Truck('RecifeTransport', 'County5', 'MT', 45.642636, -107.939221) #Other State
+		
+		trucks_bystate = defaultdict(list)
+		locator = TruckLocator(trucks_bystate)
+		
+		trucks_bystate['WY'].append(truck1)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck1)
+		trucks_bystate['WY'].append(truck2)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck2)
+		trucks_bystate['WY'].append(truck3)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck3)
+		trucks_bystate['WY'].append(truck4)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck4)
+		
+		trucks_bystate['MT'].append(truck5)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck4)
+		trucks_bystate['MT'].append(truck6)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck4)
+
+	def test_trucks_only_other_states(self):
+		truck1 = Truck('RecifeTransport', 'County1', 'MT', 48.208000, -115.365980) #Farthest
+		truck2 = Truck('RecifeTransport', 'County2', 'MT', 47.440887, -105.566175) #Near
+		truck3 = Truck('RecifeTransport', 'County3', 'SD', 44.384512, -99.523694) #Close
+		truck4 = Truck('RecifeTransport', 'County4', 'NE', 41.769037, -101.633069) #Really close
+		truck5 = Truck('RecifeTransport', 'County4', 'CO', 40.911208, -107.214124) #Really close
+
+		trucks_bystate = defaultdict(list)
+		locator = TruckLocator(trucks_bystate)
+
+		trucks_bystate['MT'].append(truck1)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck1)
+		trucks_bystate['MT'].append(truck2)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck2)
+		trucks_bystate['SD'].append(truck3)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck3)
+		trucks_bystate['NE'].append(truck4)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck4)
+		trucks_bystate['CO'].append(truck5)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck5)
+
+	def test_trucks_in_neighbours_neighbour(self):
+		truck1 = Truck('RecifeTransport', 'County1', 'NM', 32.932369, -106.005628) #Farthest
+		truck2 = Truck('RecifeTransport', 'County2', 'AZ', 36.789248, -109.257581) #Near
+		truck3 = Truck('RecifeTransport', 'County3', 'KS', 39.748937, -101.661409) #Close
+
+		trucks_bystate = defaultdict(list)
+		locator = TruckLocator(trucks_bystate)
+
+		trucks_bystate['NM'].append(truck1)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck1)
+		trucks_bystate['AZ'].append(truck2)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck2)
+		trucks_bystate['KS'].append(truck3)
+		self.assertEqual(locator.find_nearest_truck(self.cargo)[0], truck3)		
+
 
--- core/truck_locator.py
+++ core/truck_locator.py
@@ -30,58 +30,56 @@ class TruckLocator:
 
         return self._nearest_truck(self._cargo.origin_state)
 
-    def _nearest_truck(self, root_state):
+    def _nearest_truck(self, origin_state):
         """Recursive function to look for trucks in origin and nearby states
 
         This function first searches for available trucks in the cargos origin state, it than looks for
         trucks in the neighbour states from the cargo state. If trucks are found, the function selects
         the closest truck as the chosen one, if no trucks are found in the origin and neighbour states,
-        the function is recursevly called to look for trucks in the neighbour's neighbour states.
+        the function expands the search area and looks for trucks in the neighbour's neighbour states.
 
         Args:
-            root_state: Current state being checked for nearby trucks
+            origin_state: Current state being checked for nearby trucks
 
         Returns:
             A tuple in which the first element is the nearest truck to that cargo and the second
             element is the distance in Km from the truck to the cargo.
             (Truck Object, Distance)    
         """
-        nearest_truck_root_state = None
-        shortest_distance_root_state = -1.
+        nearest_truck_origin_state = None
+        shortest_distance_origin_state = -1.
         nearest_truck_neighbours = None
         shortest_distance_neighbours = -1.
 
-        if (len(self._trucks_bystate[root_state]) > 0) and (root_state not in self._visited_states) :
-            nearest_truck_root_state, shortest_distance_root_state = self._nearest_truck_in_state(root_state)
+        if (len(self._trucks_bystate[origin_state]) > 0) and (origin_state not in self._visited_states) :
+            nearest_truck_origin_state, shortest_distance_origin_state = self._nearest_truck_in_state(origin_state)
         
-        self._visited_states.add(root_state)
+        self._visited_states.add(origin_state)
 
-        nearest_truck_neighbours, shortest_distance_neighbours = self._find_trucks_in_neighbours(root_state)
+        self._search_area = NEIGHBOURS[origin_state]
+        nearest_truck_neighbours, shortest_distance_neighbours = self._find_trucks_in_neighbours()
 
-        if (shortest_distance_neighbours == -1.) and (shortest_distance_root_state == -1.):
-            for curr_neighbour in NEIGHBOURS[root_state]:
-                curr_truck, curr_distance = self._nearest_truck(curr_neighbour)
+        if (shortest_distance_neighbours == -1.) and (shortest_distance_origin_state == -1.):            
+            while shortest_distance_neighbours == -1.:
+                self._expand_search_area()
+                nearest_truck_neighbours, shortest_distance_neighbours = self._find_trucks_in_neighbours()
 
-            if (curr_distance < shortest_distance_neighbours) or (shortest_distance_neighbours == -1.):
-                nearest_truck_neighbours = curr_truck
-                shortest_distance_neighbours = curr_distance                
-
-        if shortest_distance_root_state == -1:
+        if shortest_distance_origin_state == -1:
             return nearest_truck_neighbours, shortest_distance_neighbours
         elif shortest_distance_neighbours == -1:
-            return nearest_truck_root_state, shortest_distance_root_state
+            return nearest_truck_origin_state, shortest_distance_origin_state
         
-        if shortest_distance_root_state < shortest_distance_neighbours:
-            return nearest_truck_root_state, shortest_distance_root_state
+        if shortest_distance_origin_state < shortest_distance_neighbours:
+            return nearest_truck_origin_state, shortest_distance_origin_state
         else:
             return nearest_truck_neighbours, shortest_distance_neighbours
 
 
-    def _nearest_truck_in_state(self, root_state):
+    def _nearest_truck_in_state(self, state):
         """Looks for the nearest truck relative to the given cargo
 
         Args:
-            state_truck_list: List of trucks located in a given state.
+            state: State to search for trucks
 
         Returns:
             A tuple in which the first element is the nearest truck to that cargo and the second
@@ -90,7 +88,7 @@ class TruckLocator:
         """
         nearest_truck = None
         shortest_distance = -1.
-        state_truck_list = self._trucks_bystate[root_state]
+        state_truck_list = self._trucks_bystate[state]
 
         for curr_truck in state_truck_list:
             if shortest_distance == -1.:
@@ -104,11 +102,8 @@ class TruckLocator:
 
         return nearest_truck, shortest_distance
 
-    def _find_trucks_in_neighbours(self, root_state):
-        """Looks for trucks in the given root_state neighbours
-
-        Args:
-            root_state: Current state being checked for nearby trucks
+    def _find_trucks_in_neighbours(self):
+        """Looks for trucks in the given origin_state neighbours
 
         Returns:
             A tuple in which the first element is the nearest truck to that cargo and the second
@@ -118,7 +113,10 @@ class TruckLocator:
         nearest_truck_neighbours = None
         shortest_distance_neighbours = -1.
 
-        for curr_neighbour in NEIGHBOURS[root_state]:
+        for curr_neighbour in self._search_area:
+            if(curr_neighbour not in self._trucks_bystate):
+                continue
+
             if (len(self._trucks_bystate[curr_neighbour]) == 0) and (curr_neighbour not in self._visited_states):
                 self._visited_states.add(curr_neighbour)
                 continue
@@ -132,3 +130,11 @@ class TruckLocator:
             self._visited_states.add(curr_neighbour)
 
         return nearest_truck_neighbours, shortest_distance_neighbours
+
+    def _expand_search_area(self):
+        expanded_search_area = []
+        for curr_neighbour in self._search_area:
+            for curr_neighbour_neighbour in NEIGHBOURS[curr_neighbour]:
+                if curr_neighbour_neighbour not in self._visited_states:
+                    expanded_search_area.append(curr_neighbour_neighbour)
+        self._search_area = expanded_search_area
